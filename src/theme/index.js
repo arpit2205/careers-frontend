e4c15d2ef79db4145b19eac5314d5c7a7eb42c22
Arpit Gupta <arpit.8.a@gmail.com>
2021-12-12 19:44:22 +0530

    Chakra UI setup

---
+++ src/theme/index.js
@@ -0,0 +1,10 @@
+import { extendTheme, theme as base } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "'Karla', sans-serif",
+    heading: "'Philosopher', sans-serif",
+  },
+});
+
+export default theme;
